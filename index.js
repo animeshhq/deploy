186eac7c2f75971d53f9174c708d5e2c145bc29c
Animesh <animeshhq@gmail.com>
2026-04-24 00:59:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const express = require('express')
-const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Yoooo dude wssup')
-})
-
-app.listen(25565, () => {
-  console.log('App running on port 25565')
-})
