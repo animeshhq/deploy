9f3b959b8e1a9ac10149cf89a4cd40265939799e
Animesh <animeshhq@gmail.com>
2026-04-24 01:29:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('yooooo dude wssup')
+  res.send('hi guys its me ronaldo!!')
 })
 
 app.listen(25565, () => {
